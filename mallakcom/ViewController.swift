29e93dbff31b4d26946050509f5f7783d1573acd
ahmad saleh <ahmad.salh157@gmail.com>
2019-08-25 20:09:24 +0300

    Initial Commit

---
+++ mallakcom/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mallakcom
+//
+//  Created by mohave on 8/25/19.
+//  Copyright © 2019 mallakcom. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
